b98aadc50506eaaeca805345ca4c12dccf2519bf
Stephanie Bertrand <sbertrand101@gmail.com>
2019-09-10 23:03:15 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public static void public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	TTT game = new TTT();
+	game.initializeBoard();
+	System.out.println("Tic-Tac-Toe!");
+	do
+	{
+
+	}
+}
---
+++ TTT.java
@@ -0,0 +1,13 @@
+public TTT()
+
+public void initializeBoard()
+
+public void printBoard()
+
+public boolean isBoardFull()
+
+public boolean checkForWin()
+
+private boolean checkRowsForWin()
+
+private boolaean checkColumnsForWin()
